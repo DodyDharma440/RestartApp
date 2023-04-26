83d1ee0014835ef4f25a46d6d7144d5c9d2e2264
DodyDharma440 <dharma.dody@yahoo.com>
2023-04-26 17:20:01 +0800

    onboarding screen ui

--- RestartApp/Screens/OnboardingView.swift
+++ RestartApp/Screens/OnboardingView.swift
@@ -14,18 +14,100 @@ struct OnboardingView: View {
     
     // Mark: - Body
     
-    
     var body: some View {
-        VStack(spacing: 20.0) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                isOnboarding = false
-            }) {
-                Text("Start")
-            }
-        } // VStack
+            VStack(spacing: 20.0) {
+                // Header
+                
+                Spacer()
+                
+                VStack(spacing: 0.0) {
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+It's not how much we give, but how much love we put into giving.
+""")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10.0)
+                } // Header
+                
+                // Center
+                
+                ZStack {
+                    ZStack {
+                        Circle().stroke(.white.opacity(0.2), lineWidth: 40).frame(width: 260.0, height: 260.0, alignment: .center)
+                        Circle().stroke(.white.opacity(0.2), lineWidth: 80).frame(width: 260, height: 260, alignment: .center)
+                    } // ZStack
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } // Center
+                
+                Spacer()
+                
+                // Footer
+                
+                ZStack {
+                    // 1. Background (static)
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. Call action (static)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                
+                    // 3. Capsule (dynamic width)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    
+                    // 4. Circle (draggable)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboarding = false
+                        }
+                        
+                        Spacer()
+                    } // HStack
+                } // Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
+            } // VStack
+        } // ZStack
     }
 }
 
